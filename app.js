739d6efbd67b99006d04b22722708f9f62ee623a
MuhammadAliAP <muhammedaliap&*^@gmail.com>
2023-08-14 17:05:53 +0530

    fdbk1

--- app.js
+++ app.js
@@ -5,8 +5,8 @@ var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 
 var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
-const MongoDB =require('./services/mongodbservices')
+var authenticationRouter = require('./routes/authentication');
+const MongoDB = require('./services/mongodbservices')
 MongoDB.connectToMongoDB();
 
 var app = express();
@@ -22,15 +22,15 @@ app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
-app.use('/users', usersRouter);
+app.use('/api', authenticationRouter);
 
 // catch 404 and forward to error handler
-app.use(function(req, res, next) {
+app.use(function (req, res, next) {
   next(createError(404));
 });
 
 // error handler
-app.use(function(err, req, res, next) {
+app.use(function (err, req, res, next) {
   // set locals, only providing error in development
   res.locals.message = err.message;
   res.locals.error = req.app.get('env') === 'development' ? err : {};
